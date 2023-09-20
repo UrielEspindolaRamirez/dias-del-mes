e7af2c5823c82152598f230ac1105617ad97d1c2
uriel espindola <urielespindolaramirez1000@gmail.com>
2023-09-19 18:15:32 -0600

    final

---
+++ app.js
@@ -0,0 +1,42 @@
+let mes = prompt("poner el mes para saber sus dias");
+
+switch(mes){
+    case "enero":
+        console.log("El mes de ", mes, " tiene 31 dias");
+        break;
+    case "febrero":
+        console.log("El mes de ", mes, " tiene 28 dias");
+        break;
+    case "marzo":
+        console.log("El mes de ", mes, " tiene 31 dias");
+        break;
+    case "abril":
+        console.log("El mes de ", mes, " tiene 30 dias");
+        break;
+    case "mayo":
+        console.log("El mes de ", mes, " tiene 31 dias");
+        break;
+    case "junio":
+        console.log("El mes de ", mes, " tiene 30 dias");
+        break;
+    case "julio":
+        console.log("El mes de ", mes, " tiene 31 dias");
+        break;
+    case "agosto":
+        console.log("El mes de ", mes, " tiene 31 dias");
+        break;
+    case "septiembre":
+        console.log("El mes de ", mes, " tiene 30 dias");
+        break;
+    case "octubre":
+        console.log("El mes de ", mes, " tiene 31 dias");
+        break;
+    case "noviembre":
+        console.log("El mes de ", mes, " tiene 30 dias");
+        break;
+    case "diciembre":
+        console.log("El mes de ", mes, " tiene 31 dias");
+        break;
+    default:
+        alert("el mes es incorrecto")
+}
